64f3db1745216d7bfd29e6805c109e06626d6658
seungmin Ryu <ojysep9987@naver.com>
2022-08-04 16:53:36 +0900

    Member Repository 개발

---
+++ src/main/java/com/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,35 @@
+package com.jpabook.jpashop.repository;
+
+import com.jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // spring bean 으로 등록
+public class MemberRepository {
+
+    @PersistenceContext // 엔티티 메니저( EntityManager ) 주입
+    private EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        // sql은 테이블을 대상으로 하지만 JPQL은 class를 대상으로 함
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
